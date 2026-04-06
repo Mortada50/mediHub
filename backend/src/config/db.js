f761d098a06e34cc9b7e5e815f196d5fa563eef1
Mortada50 <mortadaabdullahaltaj@gmail.com>
2026-04-07 00:21:07 +0300

    fix: update MongoDB connection to use ENV configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import {ENV} from "./env.js";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(ENV.DB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Error: ${error.message}`);
